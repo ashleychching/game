6f3dd8b392ce9a92c7f622141c02c8afcc07c4da
Tritical1 <110200988+Tritical1@users.noreply.github.com>
2023-07-11 09:59:53 -0400

    assets and button?

--- end.py
+++ end.py
@@ -8,6 +8,10 @@ screen_width = 800
 screen_height = 600
 window = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Start Screen")
+play_again = pygame.image.load('graphics/play again text.png')
+play_again = pygame.transform.scale_by(play_again, (.5, .5))
+playRect = play_again.get_rect()
+playRect.center = (screen_width / 2, screen_height / 3)
 
 def end_screen():
     while True:
@@ -19,13 +23,13 @@ def end_screen():
                 if event.key == pygame.K_SPACE:
                     return
 
-        window.fill((255, 0, 255))  # Fill the window with white color
-
+        window.fill((255, 255, 255))  # Fill the window with white color
 
         # Draw start screen elements
         # ...
-
+        window.blit(play_again, playRect.topleft)
         pygame.display.flip()
+
 
 # Call the start_screen() function when the module is executed directly
 if __name__ == "__main__":
